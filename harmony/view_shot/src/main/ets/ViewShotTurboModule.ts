39aef3231d93e983b325866fec1e01fd5d9d0a8b
wang-yuanbai <603970629@qq.com>
2024-01-05 15:20:42 +0800

    代码更新

--- harmony/view_shot/src/main/ets/ViewShotTurboModule.ts
+++ harmony/view_shot/src/main/ets/ViewShotTurboModule.ts
@@ -51,42 +51,53 @@ export class ViewShotTurboModule extends TurboModule {
   private phAccessHelper: photoAccessHelper.PhotoAccessHelper
 
   constructor(ctx: TurboModuleContext) {
-    super(ctx)
+    super(ctx);
     this.phAccessHelper = photoAccessHelper.getPhotoAccessHelper(this.ctx.uiAbilityContext);
   }
 
   captureRef(tag: number, option: Options): Promise<string> {
-    this.requestPermission()
-    return new Promise<string>((resolve, reject) => {
-      componentSnapshot.get(tag + '').then((pixmap: image.PixelMap) => {
-        this.savePhotoOnDevice('ComponentSnapshot', option, pixmap).then(uri => {
-          resolve(uri);
+    return new Promise<string>(async (resolve, reject) => {
+      if (await this.requestPermission()) {
+        componentSnapshot.get(tag + '').then((pixmap: image.PixelMap) => {
+          this.savePhotoOnDevice('ComponentSnapshot', option, pixmap).then(uri => {
+            resolve(uri);
+          }).catch(err => {
+            console.error(`componentSnapshot failed, message = ${JSON.stringify(err)}`);
+            reject(`componentSnapshot failed, message = ${JSON.stringify(err)}`);
+          })
         }).catch(err => {
-          reject(err);
+          console.error(`componentSnapshot failed, message = ${JSON.stringify(err)}`);
+          reject(`componentSnapshot failed, message = ${JSON.stringify(err)}`);
         })
-      }).catch(err => {
-        console.error('componentSnapshot failed, message = ', err);
-        reject(err)
-      })
+      } else {
+        reject(`读写权限未授权`);
+      }
     })
   }
 
   captureScreen(option: Options): Promise<string> {
     this.requestPermission()
-    return new Promise<string>((resolve, reject) => {
-      window.getLastWindow(this.ctx.uiAbilityContext).then(windowClass => {
-        windowClass.snapshot().then(pixmap => {
-          this.savePhotoOnDevice('ComponentSnapshot', option, pixmap).then(uri => {
-            resolve(uri);
+    return new Promise<string>(async (resolve, reject) => {
+      if (await this.requestPermission()) {
+        window.getLastWindow(this.ctx.uiAbilityContext).then(windowClass => {
+          windowClass.snapshot().then(pixmap => {
+            this.savePhotoOnDevice('ScreenSnapshot', option, pixmap).then(uri => {
+              resolve(uri);
+            }).catch(err => {
+              console.error(`ScreenSnapshot failed, message = ${JSON.stringify(err)}`);
+              reject(`ScreenSnapshot failed, message = ${JSON.stringify(err)}`);
+            })
           }).catch(err => {
-            reject(err);
+            console.error(`ScreenSnapshot failed, message = ${JSON.stringify(err)}`);
+            reject(`ScreenSnapshot failed, message = ${JSON.stringify(err)}`);
           })
         }).catch(err => {
-          reject(err)
+          console.error(`ScreenSnapshot failed, message = ${JSON.stringify(err)}`);
+          reject(`ScreenSnapshot failed, message = ${JSON.stringify(err)}`);
         })
-      }).catch(err => {
-        reject(err)
-      })
+      } else {
+        reject(`读写权限未授权`);
+      }
     })
   }
 
@@ -98,14 +109,26 @@ export class ViewShotTurboModule extends TurboModule {
       if (!res) {
         return;
       }
-      if (path.getParent() == this.ctx.uiAbilityContext.cacheDir) {
-        fs.unlinkSync(path)
+      if (file.getParent() == this.ctx.uiAbilityContext.cacheDir) {
+        fs.unlinkSync(path);
       }
     })
+    fs.closeSync(file);
   }
 
-  async requestPermission() {
-    await abilityAccessCtrl.createAtManager().requestPermissionsFromUser(this.ctx.uiAbilityContext, PERMISSIONS)
+  requestPermission(): Promise<boolean> {
+    return new Promise<boolean>((resolve) => {
+      abilityAccessCtrl.createAtManager()
+        .requestPermissionsFromUser(this.ctx.uiAbilityContext, PERMISSIONS).then(result => {
+        if (result.authResults[0] == 0) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      }).catch(() => {
+        resolve(false);
+      })
+    });
   }
 
   savePhotoOnDevice(title: string, option: Options, pixmap: image.PixelMap): Promise<string> {
@@ -113,11 +136,10 @@ export class ViewShotTurboModule extends TurboModule {
       let photoType = photoAccessHelper.PhotoType.IMAGE;
       let extension = option.format;
       let options: photoAccessHelper.CreateOptions = {
-        title: option.fileName ? option.fileName : title + '-' + this.getNowTime()
+        title: option.fileName ? option.fileName : title + '-' + this.getNowTime();
       }
       this.phAccessHelper.createAsset(photoType, extension, options, (err, uri) => {
         if (uri != undefined) {
-          console.info(`img uri: ${uri}`)
           const imagePacker = image.createImagePacker();
           // 设置编码输出流和编码参数
           let packOpts: image.PackingOption = { format: "image/jpeg", quality: option.quality * 100 };
@@ -128,18 +150,18 @@ export class ViewShotTurboModule extends TurboModule {
             // 编码成功，写操作
             fs.writeSync(file.fd, data);
             fs.closeSync(file);
+            resolve(uri)
             promptAction.showToast({
               message: '已成功保存至相册',
               duration: 1000
             })
-            resolve(uri)
           }).catch((error: Error) => {
-            console.error('Failed to pack the image. And the error is: ' + error);
-            reject(error)
+            console.error(`Failed to pack the image. And the error is: ${JSON.stringify(error)}`);
+            reject(`Failed to pack the image. And the error is: ${JSON.stringify(error)}`);
           })
         } else {
-          console.error('createAsset failed, message = ', err);
-          reject(err)
+          console.error(`createAsset failed, message = ${JSON.stringify(err)}`);
+          reject(`createAsset failed, message = ${JSON.stringify(err)}`);
         }
       })
     })
@@ -154,6 +176,6 @@ export class ViewShotTurboModule extends TurboModule {
     let minutes = date.getMinutes().toString();
     let seconds = date.getSeconds().toString();
     let milliseconds = date.getMilliseconds().toString();
-    return year + (month > 10 ? month : '0' + month) + (day > 10 ? day : '0' + day) + hours + minutes + seconds + milliseconds
+    return year + (month > 10 ? month : '0' + month) + (day > 10 ? day : '0' + day) + hours + minutes + seconds + milliseconds;
   }
 }
